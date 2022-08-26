ba1b5b65ca35f10bd3353c01c6945ac859012d28
Vishwanathhiremath112 <vishwanathhiremath131@gmail.com>
2022-08-26 18:48:21 +0530

    added

---
+++ Batch1-DET-2022/TestSalaryInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class TestSalaryInfo
+    {
+        public static void Main()
+        {
+            SalaryInfo e = new SalaryInfo(22807, "Vishwanath", "Belgaum", 56783, 856786, 546898);
+            Console.WriteLine($"Employee Net Salary {e.GetNetSalary()}");
+            Console.WriteLine($"Employee Net Salary {e.GetEmpGrade()}");
+
+        }
+    }
+}
